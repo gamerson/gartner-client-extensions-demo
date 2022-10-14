fc9e1e43824151234b567a0462ff20c9c4ce8b1b
Raymond Augé <raymond.auge@liferay.com>
2022-10-13 20:42:04 -0400

    wip

--- coupon-function-gogin/main.go
+++ coupon-function-gogin/main.go
@@ -15,54 +15,76 @@ import (
 
 var router = setup()
 
+func CouponIssued(c *gin.Context) {
+	authorization := c.Request.Header.Get("Authorization")
+
+	fmt.Println("Authorization: " + authorization)
+
+	jsonData, err := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	var couponObject map[string]interface{}
+	if err := json.Unmarshal(jsonData, &couponObject); err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	objectEntry := couponObject["objectEntry"].(map[string]interface{})
+	values := objectEntry["values"].(map[string]interface{})
+	issued := values["issued"].(bool)
+
+	var modifiedDate string
+	if value, isMapContainsKey := objectEntry["modifiedDate"].(string); isMapContainsKey {
+		modifiedDate = value
+	}
+
+	createDate := objectEntry["createDate"].(string)
+	code := values["code"].(string)
+	statusByUserName := objectEntry["statusByUserName"].(string)
+
+	var status = "not issued"
+	if issued {
+		status = "issued"
+	}
+
+	var updatedDate = modifiedDate
+	if updatedDate == "" {
+		updatedDate = createDate
+	}
+
+	msg := fmt.Sprintf("The status coupon '%s' changed to '%s' by '%s' at '%s'", code, status, statusByUserName, updatedDate)
+
+	fmt.Println(msg)
+
+	c.String(http.StatusOK, "OK")
+}
+
+func WorkflowAction(c *gin.Context) {
+	authorization := c.Request.Header.Get("Authorization")
+
+	fmt.Println("Authorization: " + authorization)
+
+	jsonData, err := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	msg := fmt.Sprintf("JSON: %s", jsonData)
+
+	fmt.Println(msg)
+
+	c.String(http.StatusOK, "OK")
+}
+
 func main() {
-	router.POST("/coupons/issued", func(c *gin.Context) {
-		authorization := c.Request.Header.Get("Authorization")
-
-		fmt.Println("Authorization: " + authorization)
-
-		jsonData, err := ioutil.ReadAll(c.Request.Body)
-
-		if err != nil {
-			c.String(http.StatusInternalServerError, err.Error())
-			return
-		}
-
-		var couponObject map[string]interface{}
-		if err := json.Unmarshal(jsonData, &couponObject); err != nil {
-			c.String(http.StatusInternalServerError, err.Error())
-			return
-		}
-
-		objectEntry := couponObject["objectEntry"].(map[string]interface{})
-		values := objectEntry["values"].(map[string]interface{})
-		issued := values["issued"].(bool)
-
-		var modifiedDate string
-		if value, isMapContainsKey := objectEntry["modifiedDate"].(string); isMapContainsKey {
-			modifiedDate = value
-		}
-
-		createDate := objectEntry["createDate"].(string)
-		code := values["code"].(string)
-		statusByUserName := objectEntry["statusByUserName"].(string)
-
-		var status = "not issued"
-		if issued {
-			status = "issued"
-		}
-
-		var updatedDate = modifiedDate
-		if updatedDate == "" {
-			updatedDate = createDate
-		}
-
-		msg := fmt.Sprintf("The status coupon '%s' changed to '%s' by '%s' at '%s'", code, status, statusByUserName, updatedDate)
-
-		fmt.Println(msg)
-
-		c.String(http.StatusOK, "OK")
-	})
+	router.POST("/coupons/issued", CouponIssued)
+	router.POST("/workflow/action", WorkflowAction)
 
 	router.Run()
 }
